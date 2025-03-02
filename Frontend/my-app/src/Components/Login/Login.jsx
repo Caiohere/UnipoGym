13b3f07950cd82f3d049e9fb1936f93d23765130
CaioHere <caiohenry422@gmail.com>
2025-03-02 18:11:59 -0300

    Primeiro Commit - Login Page

---
+++ Frontend/my-app/src/Components/Login/Login.jsx
@@ -0,0 +1,83 @@
+import { FaUser, FaLock } from 'react-icons/fa';
+import React, { useState } from "react";
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "../../firebase";
+
+import "./Login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");  
+  const [password, setPassword] = useState("");  
+  const [isRegistering, setIsRegistering] = useState(false); // Para controlar se é login ou registro
+
+  // Função para lidar com login ou registro
+  async function handleAuth(event) {
+    event.preventDefault(); // Impede o comportamento padrão de recarregar a página
+
+    if (isRegistering) {
+      // Registro de novo usuário
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, username, password);
+        console.log("Usuário registrado:", userCredential.user);
+        alert("Registro efetuado com sucesso!")
+      } catch (error) {
+        console.error("Erro ao registrar:", error.message);
+        alert(error.message);
+      }
+    } else {
+      // Login de usuário existente
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, username, password);
+        console.log("Usuário logado:", userCredential.user);
+        alert("Login efetuado com sucesso!")
+      } catch (error) {
+        console.error("Erro ao fazer login:", error.message);
+        alert(error.message);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={handleAuth}>
+          <h1>UNIPO GYM</h1>
+
+          <div className="input-field">
+            <input 
+              type="email" 
+              placeholder="E-mail" 
+              onChange={(e) => setUsername(e.target.value)} 
+              value={username} // Controle de estado
+              required 
+            />
+            <FaUser className="icon" />
+          </div>
+
+          <div className="input-field">
+            <input 
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}  
+              value={password} // Controle de estado
+              required
+            />
+            <FaLock className="icon" />
+          </div>
+
+          <button type="submit">{isRegistering ? "Registrar" : "Entrar"}</button>
+
+          <div className="signup-link">
+            <p>
+              {isRegistering ? "Já tem uma conta? " : "Criar conta? "}
+              <a href="#" onClick={() => setIsRegistering(!isRegistering)}>
+                {isRegistering ? "Fazer login" : "Registrar"}
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
